5a66e7169e1f0b4cd4b548da93b293cc6431a7e6
Chottek <49697145+Chottek@users.noreply.github.com>
2021-01-11 10:57:12 +0100

    Added button to generate random numbers onclick

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -2,14 +2,27 @@ import React from "react"
 
 export default function Generator(){
 
-    return Array.apply(null, { length: 10 }).map((e, i) => (
-        <li>{generate(0, 100)}</li>
-    ));
+    const [num, setRandomly] = React.useState([])
 
+    React.useEffect(() => { nums() }, []);
+
+    let nums = () => {
+        setRandomly(Array.apply(0, Array(10)).map(() => generate(0, 100)));
+    }
+
+    return(
+        <>
+            <button onClick={nums}>Generate Random List</button><br/><br/>
+            { Array.apply(null, { length: 10 }).map((e, i) => (
+                <li>{generate(0, 100)}</li>
+            ))}
+        </>
+    )
 }
-
 
 function generate(min, max){
     min = Math.ceil(min);
     return Math.floor(Math.random() * (Math.floor(max) - min + 1)) + min;
+
+
 }
