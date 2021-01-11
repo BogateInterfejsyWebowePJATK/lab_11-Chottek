a4b028cbaffd0e4616786832f35f65e66b37c8ad
Chottek <49697145+Chottek@users.noreply.github.com>
2021-01-11 08:35:14 +0100

    Sth's not workin

---
+++ src/components/Generator.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Generator(){
+
+    return Array.apply(null, { length: 10 }).map((e, i) => (
+        <li>{generate(0, 100)}</li>
+    ));
+
+}
+
+function generate(min, max){
+    min = Math.ceil(min);
+    return Math.floor(Math.random() * (Math.floor(max) - min + 1)) + min;
+}
